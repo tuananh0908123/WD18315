969fc70cb3d6f95922bd4b82ea45d470ca06f575
tuananh09082004123 <tuananh09082004@gmail.com>
2024-06-29 15:54:26 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\sinhVien;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+///GET, POST , PUT ,PATCH, DELETE (method htpp) 
+/// Base url :http://127.0.0.1:8000/
+        // Dat ten : danh-sach-san-pham
+Route::get('test',function(){
+    echo '123';
 });
+Route::get('/',function(){
+    echo 'Trang chu';
+});
+Route::get('list-products', [ProductController::class,'showProduct']);
+Route::get('get-product/{id}/{name?}',[ProductController::class,'getProduct']);
+Route::get('update-product',[ProductController::class,'updateProduct']);
+Route::get('thongtinsv', [SinhVien::class, 'showStudentInfo']);
